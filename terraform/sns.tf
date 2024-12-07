2428b78cec36f38cbe853310079e561f397a74a5
Maran <119050451+Maran25@users.noreply.github.com>
2024-12-07 11:55:49 +0530

    queue subscription added to the sns

--- terraform/sns.tf
+++ terraform/sns.tf
@@ -1,3 +1,10 @@
 resource "aws_sns_topic" "sns_topic" {
   name = "test_sns_topic"
 }
+
+resource "aws_sns_topic_subscription" "queue_url_subscription" {
+  topic_arn = aws_sns_topic.sns_topic.arn
+  endpoint = aws_sqs_queue.sqs_queue.arn
+  id = "test_queue_sns_topic_subscription"
+  protocol = "sqs"
+}
