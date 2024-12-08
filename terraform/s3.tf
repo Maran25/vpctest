4f1ac8663c326d78144eb4b256ed66de58761610
Maran <119050451+Maran25@users.noreply.github.com>
2024-12-08 19:24:56 +0530

    bucket policy added

--- terraform/s3.tf
+++ terraform/s3.tf
@@ -1,3 +1,37 @@
 resource "aws_s3_bucket" "cloudtrail_bucket" {
   bucket = "cloudtrail-logs-${var.account_id}"
 }
+
+# S3 Bucket Policy for CloudTrail
+resource "aws_s3_bucket_policy" "cloudtrail_bucket_policy" {
+  bucket = aws_s3_bucket.cloudtrail_bucket.id
+
+  policy = jsonencode({
+    Version = "2012-10-17",
+    Statement = [
+      {
+        Sid       = "AWSCloudTrailAclCheck"
+        Effect    = "Allow"
+        Principal = {
+          Service = "cloudtrail.amazonaws.com"
+        }
+        Action = "s3:GetBucketAcl"
+        Resource = "arn:aws:s3:::${aws_s3_bucket.cloudtrail_bucket.id}"
+      },
+      {
+        Sid       = "AWSCloudTrailWrite"
+        Effect    = "Allow"
+        Principal = {
+          Service = "cloudtrail.amazonaws.com"
+        }
+        Action = "s3:PutObject"
+        Resource = "arn:aws:s3:::${aws_s3_bucket.cloudtrail_bucket.id}/AWSLogs/${var.account_id}/*"
+        Condition = {
+          StringEquals = {
+            "s3:x-amz-acl" = "bucket-owner-full-control"
+          }
+        }
+      }
+    ]
+  })
+}
