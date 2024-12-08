16001734781b71279725d6d8e30da4074707a4d9
Maran <119050451+Maran25@users.noreply.github.com>
2024-12-08 11:18:17 +0530

    filter policy removed

--- terraform/lambda_policy.tf
+++ terraform/lambda_policy.tf
@@ -1,3 +1,19 @@
+resource "aws_iam_role" "lambda_execution_role" {
+  name = "lambda-execution-role"
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17",
+    Statement: [
+      {
+        Effect: "Allow",
+        Principal: {
+          Service: "lambda.amazonaws.com"
+        },
+        Action: "sts:AssumeRole"
+      }
+    ]
+  })
+}
+
 resource "aws_iam_policy" "lambda_policy" {
   name = "lambda-policy"
   policy = jsonencode({
